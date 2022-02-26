87ac5eb29c72c6ce32b6256747f0b4dba71fa994
midCenturyDude7 <mgriffes@gmail.com>
2022-02-26 15:10:39 -0800

    Completed Explore and Post pages

--- src/components/shared/FollowSuggestions.js
+++ src/components/shared/FollowSuggestions.js
@@ -1,33 +1,35 @@
 import React from "react";
-import { Avatar, Typography } from "@material-ui/core";
-import { LoadingLargeIcon } from "../../icons";
 import { useFollowSuggestionsStyles } from "../../styles";
+import { Typography, Avatar } from "@material-ui/core";
+import { LoadingLargeIcon } from "../../icons";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { getDefaultUser } from "../../data";
 import { Link } from "react-router-dom";
 import FollowButton from "./FollowButton";
 
-function FollowSuggestions() {
+function FollowSuggestions({ hideHeader }) {
   const classes = useFollowSuggestionsStyles();
 
   let loading = false;
 
   return (
     <div className={classes.container}>
-      <Typography 
-        color="textSecondary"
-        variant="subtitle2"
-        className={classes.typography}
-      >
-        Suggestions For You
-      </Typography>
+      {!hideHeader && (
+        <Typography
+          color="textSecondary"
+          variant="subtitle2"
+          className={classes.typography}
+        >
+          Suggestions For You
+        </Typography>
+      )}
       {loading ? (
         <LoadingLargeIcon />
-       ) : (
-        <Slider 
-          className = {classes.slide}
+      ) : (
+        <Slider
+          className={classes.slide}
           dots={false}
           infinite
           speed={1000}
@@ -38,25 +40,25 @@ function FollowSuggestions() {
           slidesToScroll={3}
           easing="ease-in-out"
         >
-          {Array.from({ length: 10}, () => getDefaultUser()).map(user => (
+          {Array.from({ length: 10 }, () => getDefaultUser()).map(user => (
             <FollowSuggestionsItem key={user.id} user={user} />
           ))}
         </Slider>
-       )}
+      )}
     </div>
-  )
+  );
 }
 
 function FollowSuggestionsItem({ user }) {
   const classes = useFollowSuggestionsStyles();
-  const { profile_image, username, name } = user
+  const { profile_image, username, name } = user;
 
   return (
     <div>
       <div className={classes.card}>
         <Link to={`/${username}`}>
-          <Avatar 
-            scr={profile_image}
+          <Avatar
+            src={profile_image}
             alt={`${username}'s profile`}
             classes={{
               root: classes.avatar,
@@ -73,7 +75,12 @@ function FollowSuggestionsItem({ user }) {
             {username}
           </Typography>
         </Link>
-        <Typography color="textSecondary" variant="body2" className={classes.text} align="center">
+        <Typography
+          color="textSecondary"
+          variant="body2"
+          className={classes.text}
+          align="center"
+        >
           {name}
         </Typography>
         <FollowButton side={false} />
