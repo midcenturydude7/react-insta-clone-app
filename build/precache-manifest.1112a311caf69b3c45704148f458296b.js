ff0e8d85f84d7c7893027cf66c665452a166eb7d
midCenturyDude7 <mgriffes@gmail.com>
2022-02-27 22:04:57 -0800

    Deployment files

---
+++ build/precache-manifest.1112a311caf69b3c45704148f458296b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63501b3b197110ae3bd2a434717dbc21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0afed389b959f1f640c",
+    "url": "/static/css/2.c60feeff.chunk.css"
+  },
+  {
+    "revision": "d0afed389b959f1f640c",
+    "url": "/static/js/2.eb2b73ee.chunk.js"
+  },
+  {
+    "revision": "e8ccc7cf4c855a56e3a7079a2b405db4",
+    "url": "/static/js/2.eb2b73ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a1bfeabf59fce10cd6",
+    "url": "/static/js/3.4c5033ad.chunk.js"
+  },
+  {
+    "revision": "85772b11bee4bf36b18a",
+    "url": "/static/js/main.3cd75439.chunk.js"
+  },
+  {
+    "revision": "073aacf66fcc6788303d",
+    "url": "/static/js/runtime-main.117c4e70.js"
+  },
+  {
+    "revision": "cbb9b56a5cf892318fc8ab502f9d3ba7",
+    "url": "/static/media/facebook-icon-blue.cbb9b56a.svg"
+  },
+  {
+    "revision": "a9a13771e5dcd73931e0b61df2faac62",
+    "url": "/static/media/icon-sheet-2.a9a13771.png"
+  },
+  {
+    "revision": "3dfb71b4bb35ca8b35728b9475b7d861",
+    "url": "/static/media/icon-sheet.3dfb71b4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
